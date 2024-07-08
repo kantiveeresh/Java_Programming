dd7695960a39ccbadbca1e504ccf3f3d42c94dc7
kantiveeresh <96222882+kantiveeresh@users.noreply.github.com>
2024-07-08 07:30:25 +0530

    Add files via upload

---
+++ DecrementalOperator.java
@@ -0,0 +1,34 @@
+package day3;
+
+public class DecrementalOperator {
+
+	public static void main(String[] args) {
+		
+		/*
+		int a=10;
+		int res=a--1;
+		System.out.println(res); //9
+		*/
+		
+		//case2 - post decrement
+		/*int a=100;
+		
+		int resu=a--;
+		System.out.println(resu); //100
+		System.out.println(a);	  //99
+		*/
+		
+		//case3 - pre decrement
+		int a=200;
+		
+		int resul = --a;
+		System.out.println(resul);  //199
+		System.out.println(a);      //199
+		
+		
+		
+		
+		
+	}
+
+}
